c38c56418e51093ba8f888d193bf2e367530380f
MARITZA ELIZABETH LOPEZ PEREZ <maritzalopez1129928@correo.itm.edu.co>
2025-11-14 16:51:51 -0500

    AutoMapper created Dtos created

---
+++ APIMovies2/DAL/Models/CategotyCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIMovies2.DAL.Models.Dtos
+{
+    public class CategotyCreateDto
+    {
+        [Required(ErrorMessage = "El nombre de la categoria es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El numero de la categoria es de 100.")]
+        public string Name { get; set; }    
+    }
+}
---
+++ APIMovies2/DAL/Models/Dtos/CetegoryDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace APIMovies2.DAL.Models.Dtos
+{
+    public class CetegoryDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre de la categoria es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El numero de la categoria es de 100.")]
+        public string Name { get; set; }    
+
+        public DateTime CreatedDate { get; set; }   
+        public DateTime ModifiedDate { get; set; }
+
+    }
+}

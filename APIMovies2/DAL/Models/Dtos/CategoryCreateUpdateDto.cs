5dcf1becd5c3eaa29773800b7e6c6599bdf59f05
maritzalopez1989-bit <maritza.lopez1989@gmail.com>
2025-11-26 03:20:43 +0000

    ajuste del createUpdate

--- APIMovies2/DAL/Models/Dtos/CategoryCreateUpdateDto.cs
+++ APIMovies2/DAL/Models/Dtos/CategoryCreateUpdateDto.cs
@@ -2,10 +2,11 @@
 
 namespace APIMovies2.DAL.Models.Dtos
 {
-    public class CategoryCreateDto
+    public class CategoryCreateUpdateDto
     {
         [Required(ErrorMessage = "El nombre de la categoría es obligatorio.")]
         [MaxLength(100, ErrorMessage = "El número máximo de caracteres es de 100.")]
         public string Name { get; set; }
     }
 }
+
